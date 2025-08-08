b4cdf8a3ebd7f87298c88048adb1bc6934d60546
Dmytro Papiiants <papiyants@gmail.com>
2025-08-08 10:31:14 +0200

    add: markup of ProductCard component in ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -14,45 +14,43 @@ const ProductCard = (props) => {
     <div
       id={product.id}
       key={product.id}
-      className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+      className="product-card"
     >
-      <div className="card text-center h-100" key={product.id}>
-        <img
-          className="card-img-top p-3"
-          src={product.image}
-          alt="Card"
-          height={300}
-        />
-        <div className="card-body">
-          <h5 className="card-title">
-            {product.title.substring(0, 12)}...
-          </h5>
-          <p className="card-text">
-            {product.description.substring(0, 90)}...
-          </p>
+      <img
+        class="product-card__image"
+        src={product.image}
+        alt={product.title}
+        width="300"
+        height="200"
+        loading="lazy"
+      />
+
+      <div className="product-card__body">
+        <h3 className="product-card__title">{product.title.substring(0, 12)}...</h3>
+        <div className="product-card__description">
+          <p>{product.description.substring(0, 90)}...</p>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item lead">$ {product.price}</li>
-          {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-        </ul>
-        <div className="card-body">
-          <Link
-            to={"/product/" + product.id}
-            className="btn btn-dark m-1"
-          >
-            Buy Now
-          </Link>
-          <button
-            className="btn btn-dark m-1"
-            onClick={() => {
-              toast.success("Added to cart");
-              addProduct(product);
-            }}
-          >
-            Add to Cart
-          </button>
+        <div className="product-card__price">
+          <span className="product-card__price-currency">$</span>
+          {product.price}
         </div>
+      </div>
+      <div className="product-card__actions">
+        <Link
+          className="product-card__button product-card__button--link"
+          to={"/product/" + product.id}
+        >
+          More information
+        </Link>
+        <button
+          className="product-card__button product-card__button--action"
+          onClick={() => {
+            toast.success("Product added to cart")
+            addProduct(product)
+          }}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   )
